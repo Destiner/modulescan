cfe54f864572b0016a0c454b88a3161e329c5311
Timur Badretdinov <accounts@destiner.io>
2024-10-26 15:39:03 +0100

    feat(hyperindex): add ERC7579Account contract

--- packages/hyperindex/src/entryPoint.ts
+++ packages/hyperindex/src/entryPoint.ts
@@ -6,6 +6,9 @@ import {
   handlerContext,
 } from "generated";
 
+const KERNEL_V3_META_FACTORY_ADDRESS =
+  "0xd703aae79538628d27099b8c4f621be4ccd142d5";
+
 EntryPointV0_7_0.AccountDeployed.contractRegister(({ event, context }) => {
   handleAccountDeployedContractRegister(event, context);
 });
@@ -16,6 +19,10 @@ function handleAccountDeployedContractRegister(
 ) {
   const address = event.params.sender.toLowerCase();
   const factory = event.params.factory.toLowerCase();
+
+  if (factory === KERNEL_V3_META_FACTORY_ADDRESS) {
+    context.addERC7579Account(address);
+  }
 }
 
 EntryPointV0_7_0.AccountDeployed.handler(async ({ event, context }) => {
