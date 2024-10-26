880af86696a2dd4a4d621d7e1af381a3579a3dff
Timur Badretdinov <accounts@destiner.io>
2024-10-26 12:22:09 +0100

    feat(hyperindex): index account deployments

---
+++ packages/hyperindex/src/entryPoint.ts
@@ -0,0 +1,38 @@
+import {
+  contractRegistrations,
+  EntryPointV0_7_0_AccountDeployed_eventArgs,
+  EntryPointV0_7_0,
+  eventLog,
+  handlerContext,
+} from "generated";
+
+EntryPointV0_7_0.AccountDeployed.contractRegister(({ event, context }) => {
+  handleAccountDeployedContractRegister(event, context);
+});
+
+function handleAccountDeployedContractRegister(
+  event: eventLog<EntryPointV0_7_0_AccountDeployed_eventArgs>,
+  context: contractRegistrations
+) {
+  const address = event.params.sender.toLowerCase();
+  const factory = event.params.factory.toLowerCase();
+}
+
+EntryPointV0_7_0.AccountDeployed.handler(async ({ event, context }) => {
+  handleAccountDeployedEvent(event, context);
+});
+
+async function handleAccountDeployedEvent(
+  event: eventLog<EntryPointV0_7_0_AccountDeployed_eventArgs>,
+  context: handlerContext
+) {
+  const factory = event.params.factory;
+  const address = event.params.sender.toLowerCase();
+
+  context.Account.set({
+    id: `${event.chainId}-${address}`,
+    chainId: event.chainId,
+    address,
+    factory,
+  });
+}
