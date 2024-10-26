35cddf7a1cd31638cf9607335e9db8d1b6bf6b78
Timur Badretdinov <accounts@destiner.io>
2024-10-26 19:52:28 +0100

    feat(hyperindex): track module uninstallation

--- packages/hyperindex/src/erc7579Account.ts
+++ packages/hyperindex/src/erc7579Account.ts
@@ -1,5 +1,6 @@
 import {
   ERC7579Account_ModuleInstalled_eventArgs,
+  ERC7579Account_ModuleUninstalled_eventArgs,
   ERC7579Account,
   eventLog,
   handlerContext,
@@ -7,6 +8,10 @@ import {
 
 ERC7579Account.ModuleInstalled.handler(async ({ event, context }) => {
   handleModuleInstalledEvent(event, context);
+});
+
+ERC7579Account.ModuleUninstalled.handler(async ({ event, context }) => {
+  handleModuleUninstalledEvent(event, context);
 });
 
 async function handleModuleInstalledEvent(
@@ -19,5 +24,23 @@ async function handleModuleInstalledEvent(
   if (account) {
     account.modules.push(moduleAddress);
     context.Account.set(account);
+  }
+}
+
+async function handleModuleUninstalledEvent(
+  event: eventLog<ERC7579Account_ModuleUninstalled_eventArgs>,
+  context: handlerContext
+) {
+  const address = event.srcAddress.toLowerCase();
+  const moduleAddress = event.params.module.toLowerCase();
+  const account = await context.Account.get(`${event.chainId}-${address}`);
+  if (account) {
+    context.Account.set({
+      id: account.id,
+      chainId: account.chainId,
+      address: account.address,
+      factory: account.factory,
+      modules: account.modules.filter((m: string) => m !== moduleAddress),
+    });
   }
 }
