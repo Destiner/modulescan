a7802324d9acbe6069e3b3fb17353ac407a2d1e2
Timur Badretdinov <accounts@destiner.io>
2024-10-26 21:07:10 +0100

    feat(hyperindex): add ModuleUninstalled to config

--- packages/hyperindex/src/erc7579Account.ts
+++ packages/hyperindex/src/erc7579Account.ts
@@ -1,6 +1,6 @@
 import {
   ERC7579Account_ModuleInstalled_eventArgs,
-  // ERC7579Account_ModuleUninstalled_eventArgs,
+  ERC7579Account_ModuleUninstalled_eventArgs,
   ERC7579Account,
   eventLog,
   handlerContext,
@@ -10,9 +10,9 @@ ERC7579Account.ModuleInstalled.handler(async ({ event, context }) => {
   handleModuleInstalledEvent(event, context);
 });
 
-// ERC7579Account.ModuleUninstalled.handler(async ({ event, context }) => {
-//   handleModuleUninstalledEvent(event, context);
-// });
+ERC7579Account.ModuleUninstalled.handler(async ({ event, context }) => {
+  handleModuleUninstalledEvent(event, context);
+});
 
 async function handleModuleInstalledEvent(
   event: eventLog<ERC7579Account_ModuleInstalled_eventArgs>,
@@ -60,32 +60,30 @@ async function handleModuleInstalledEvent(
   });
 }
 
-// async function handleModuleUninstalledEvent(
-//   event: eventLog<ERC7579Account_ModuleUninstalled_eventArgs>,
-//   context: handlerContext
-// ) {
-//   const address = event.srcAddress.toLowerCase();
-//   const moduleAddress = event.params.module.toLowerCase();
-//   const moduleTypeId = event.params.moduleTypeId;
-//   const accountId = `${event.chainId}-${address}`;
-
-//   const installedModuleId = `${event.chainId}-${address}-${moduleAddress}-${moduleTypeId}`;
-//   const installedModule = await context.InstalledModule.get(installedModuleId);
-//   if (installedModule) {
-//     context.InstalledModule.deleteUnsafe(installedModuleId);
-//   }
-
-//   const txHash = event.transaction.hash;
-//   const logIndex = event.logIndex;
-//   context.ModuleUninstallation.set({
-//     id: `${event.chainId}-${txHash}-${logIndex}`,
-//     chainId: event.chainId,
-//     account_id: accountId,
-//     accountModule_id: `${event.chainId}-${moduleAddress}`,
-//     moduleType: moduleTypeId,
-//     blockNumber: event.block.number,
-//     txHash: txHash,
-//     logIndex: logIndex,
-//     timestamp: event.block.timestamp,
-//   });
-// }
+async function handleModuleUninstalledEvent(
+  event: eventLog<ERC7579Account_ModuleUninstalled_eventArgs>,
+  context: handlerContext
+) {
+  //   const address = event.srcAddress.toLowerCase();
+  //   const moduleAddress = event.params.module.toLowerCase();
+  //   const moduleTypeId = event.params.moduleTypeId;
+  //   const accountId = `${event.chainId}-${address}`;
+  //   const installedModuleId = `${event.chainId}-${address}-${moduleAddress}-${moduleTypeId}`;
+  //   const installedModule = await context.InstalledModule.get(installedModuleId);
+  //   if (installedModule) {
+  //     context.InstalledModule.deleteUnsafe(installedModuleId);
+  //   }
+  //   const txHash = event.transaction.hash;
+  //   const logIndex = event.logIndex;
+  //   context.ModuleUninstallation.set({
+  //     id: `${event.chainId}-${txHash}-${logIndex}`,
+  //     chainId: event.chainId,
+  //     account_id: accountId,
+  //     accountModule_id: `${event.chainId}-${moduleAddress}`,
+  //     moduleType: moduleTypeId,
+  //     blockNumber: event.block.number,
+  //     txHash: txHash,
+  //     logIndex: logIndex,
+  //     timestamp: event.block.timestamp,
+  //   });
+}
