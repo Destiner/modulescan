e877b891d0add4a72e7eb502a0b14178410d7597
Timur Badretdinov <accounts@destiner.io>
2024-10-26 12:53:50 +0100

    remove indexing logic

--- packages/hyperindex/src/entryPoint.ts
+++ packages/hyperindex/src/entryPoint.ts
@@ -6,33 +6,33 @@ import {
   handlerContext,
 } from "generated";
 
-EntryPointV0_7_0.AccountDeployed.contractRegister(({ event, context }) => {
-  handleAccountDeployedContractRegister(event, context);
-});
+// EntryPointV0_7_0.AccountDeployed.contractRegister(({ event, context }) => {
+//   handleAccountDeployedContractRegister(event, context);
+// });
 
-function handleAccountDeployedContractRegister(
-  event: eventLog<EntryPointV0_7_0_AccountDeployed_eventArgs>,
-  context: contractRegistrations
-) {
-  const address = event.params.sender.toLowerCase();
-  const factory = event.params.factory.toLowerCase();
-}
+// function handleAccountDeployedContractRegister(
+//   event: eventLog<EntryPointV0_7_0_AccountDeployed_eventArgs>,
+//   context: contractRegistrations
+// ) {
+//   const address = event.params.sender.toLowerCase();
+//   const factory = event.params.factory.toLowerCase();
+// }
 
-EntryPointV0_7_0.AccountDeployed.handler(async ({ event, context }) => {
-  handleAccountDeployedEvent(event, context);
-});
+// EntryPointV0_7_0.AccountDeployed.handler(async ({ event, context }) => {
+//   handleAccountDeployedEvent(event, context);
+// });
 
-async function handleAccountDeployedEvent(
-  event: eventLog<EntryPointV0_7_0_AccountDeployed_eventArgs>,
-  context: handlerContext
-) {
-  const factory = event.params.factory;
-  const address = event.params.sender.toLowerCase();
+// async function handleAccountDeployedEvent(
+//   event: eventLog<EntryPointV0_7_0_AccountDeployed_eventArgs>,
+//   context: handlerContext
+// ) {
+//   const factory = event.params.factory;
+//   const address = event.params.sender.toLowerCase();
 
-  context.Account.set({
-    id: `${event.chainId}-${address}`,
-    chainId: event.chainId,
-    address,
-    factory,
-  });
-}
+//   context.Account.set({
+//     id: `${event.chainId}-${address}`,
+//     chainId: event.chainId,
+//     address,
+//     factory,
+//   });
+// }
